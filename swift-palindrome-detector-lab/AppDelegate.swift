55d45e8ef13c0222156367f3a5bd4e63f7460b1b
MadinaIbrahim <madina.ibrahim.us@gmail.com>
2016-09-23 23:41:37 -0400

    Done

--- swift-palindrome-detector-lab/AppDelegate.swift
+++ swift-palindrome-detector-lab/AppDelegate.swift
@@ -10,37 +10,58 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    
+    
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        let stringIsPalindromeVar1 = stringIsPalindrome("racecar")
+        print("Testing(1) if 'racecar' is \(stringIsPalindromeVar1) palindrome.")
+        
+        let stringIsPalindromeVar2 = stringIsPalindrome("racecars")
+        print("Testing(2) if 'racecars' is \(stringIsPalindromeVar2) palindrome.")
+        
+        let stringIsPalindromeVar3 = stringIsPalindrome("Kanakanak")
+        print("Testing(3) if 'Kanakanak' with mixed upper and lowercase letters is \(stringIsPalindromeVar3) palindrome.")
+        
+        let stringIsPalindromeVar4 = stringIsPalindrome("never odd or even")
+        print("Testing(4) if 'never odd or even' string contaning spaces is \(stringIsPalindromeVar4) palindrome.")
+        
+        let stringIsPalindromeVar5 = stringIsPalindrome("this is not a palindrome")
+        print("Testing(5) if 'this is not a palindrome' string contaning spaces is \(stringIsPalindromeVar5) palindrome.")
+        
+        var stringByReversing =  stringByReversingString("racecars")
+        print("The reversed 'racecars' string is '\(stringByReversing)'.")
+        
+        stringByReversing =  stringByReversingString("racecar")
+        print("The reversed 'racecar' string is '\(stringByReversing)'.")
+        
+        return false
+    }
+    
+    
+    func stringByReversingString(string: String) -> String {
+        
+        let reversedString = String(string.characters.reverse())
+        print(" '\(reversedString)' is a string we got by reversing string '\(string)'.")
+        return reversedString
+    }
+    
+    func stringIsPalindrome(string: String) -> Bool {
+        
+        let reversedString = String(string.characters.reverse())
+        print(reversedString)
+        
+        let reversedWhitespaceRemovedString = string.stringByReplacingOccurrencesOfString(" ", withString: "")
+        print("String with trimmed whitespace '\(reversedWhitespaceRemovedString)'.")
+        
+        if reversedWhitespaceRemovedString.lowercaseString != string.lowercaseString {
+            return false
+        }
+        
         return true
     }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
+    
 }
 
